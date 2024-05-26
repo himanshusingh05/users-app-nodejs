581be5f273b1423487dc940ee580892c728fe6f5
kkoollhim <hs3045@gmail.com>
2024-05-26 15:05:34 +0530

    first commit

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var dbConn  = require('../lib/db');
  
 // display user page
-router.get('/', function(req, res, next) {      
+router.get('/', function(req, res, next) {     
     dbConn.query('SELECT * FROM users ORDER BY id desc',function(err,rows)     {
         if(err) {
             req.flash('error', err);
